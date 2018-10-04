2694bcab316f46d7bd22723c424c0af6bcfc967b
David Chambers <dlchambersjr@gmail.com>
2018-10-04 12:24:19 -0700

    save point before lunch

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/explore', getSQL);
 app.get('/details/:id', getCountry);
 
 //Set the catch all route
-app.get('*', (request, response) => response.status(404).render('pages/404-error.ejs', { errorResult: '`can\'t load a page' }));
+app.get('*', (request, response) => response.status(404).render('pages/404-error.ejs', { errorResult: '' }));
 
 // Activate the server
 app.listen(PORT, () => console.log(`(TRAVEL BUDDY) listening on: ${PORT}`));
@@ -65,16 +65,16 @@ app.listen(PORT, () => console.log(`(TRAVEL BUDDY) listening on: ${PORT}`));
 // +++++++++++++++++++++++++++++++++
 
 function Countries(data) {
-    this.id = data.id;
-    this.country_name = data.country_name;
-    this.capital = data.capital;
-    this.country_code = data.country_code;
-    this.currency_code = data.currency_code;
-    this.exchange_rate = data.exchange_rate;
-    this.local_bmi = data.local_bmi;
-    this.usa_bmi = data.usa_bmi;
-    this.flag_url = data.flag_url;
-    this.created_date = Date.now();
+  this.id = data.id;
+  this.country_name = data.country_name;
+  this.capital = data.capital;
+  this.country_code = data.country_code;
+  this.currency_code = data.currency_code;
+  this.exchange_rate = data.exchange_rate;
+  this.local_bmi = data.local_bmi;
+  this.usa_bmi = data.usa_bmi;
+  this.flag_url = data.flag_url;
+  this.created_date = Date.now();
 }
 
 Countries.allCountries = [];
@@ -87,126 +87,125 @@ function renderHomePage(request, response) { response.render('index'); }
 
 // Get the API info for currency from fixer.io and returns an array of arrays with currency code and exchange rate in each array.
 function getCurrency() {
-    console.log('** Retrieving Currency from API');
-    const url = `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&base=USD`;
+  console.log('** Retrieving Currency from API');
+  const url = `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&base=USD`;
 
-    return superagent(url)
-        .then(result => {
-            let ratesArray = Object.entries(result.body.rates);
-            return ratesArray;
-        })
+  return superagent(url)
+    .then(result => {
+      let ratesArray = Object.entries(result.body.rates);
+      return ratesArray;
+    })
 }
 
 // Get API info from RESTcountries to populate capital and flag_url info.  The function will receive information from our SQL database to limit the countries being requested.
 
 function getCapitalsAndFlags(data) {
-    // For each country code we will add the code to a variable that appends to the end of the Restcountries API.
-    console.log('*** Retrieving Capitals and Flags from API');
+  // For each country code we will add the code to a variable that appends to the end of the Restcountries API.
+  console.log('*** Retrieving Capitals and Flags from API');
 
-    let countryCodes = '';
+  let countryCodes = '';
 
-    data.forEach(country => {
-        countryCodes += `${country.country_code};`;
+  data.forEach(country => {
+    countryCodes += `${country.country_code};`;
+  })
+
+  const url = `https://restcountries.eu/rest/v2/alpha?codes=${countryCodes}`;
+
+  return superagent(url)
+    .then(result => {
+      let capitalArray = [];
+      result.body.forEach(country => {
+        capitalArray.push([country.alpha3Code, country.capital, country.flag]);
+      })
+      return capitalArray;
     })
-
-    const url = `https://restcountries.eu/rest/v2/alpha?codes=${countryCodes}`;
-
-    return superagent(url)
-        .then(result => {
-            let capitalArray = [];
-            result.body.forEach(country => {
-                capitalArray.push([country.alpha3Code, country.capital, country.flag]);
-            })
-            return capitalArray;
-        })
-        .catch(err => processErrors(err));
+    .catch(err => processErrors(err));
 }
 
 // Retrieve the data from the SQL server and update it with the API info
 function getSQL(request, response) {
-    console.log('* Retrieving stored data from SQL Server');
+  console.log('* Retrieving stored data from SQL Server');
 
-    const SQL = `SELECT * FROM countries;`;
+  const SQL = `SELECT * FROM countries;`;
 
-    let countriesDB = [];
-    let currency = [];
-    let capitalsAndFlags = [];
-    Countries.allCountries = [];
+  let countriesDB = [];
+  let currency = [];
+  let capitalsAndFlags = [];
+  Countries.allCountries = [];
 
-    client.query(SQL)
-        // First get the data from the SQL server
-        .then(results => countriesDB = results.rows)
-        // Use the SQL data to help get capitals and flags
-        .then(countries => getCapitalsAndFlags(countries)
-            .then(capsAndFlags => capitalsAndFlags = capsAndFlags))
-        // Get the current currency rates
-        .then(getCurrency()
-            .then(rates => currency = rates))
-        // Update data with the retrived information and calculate the current Big Mac Index for each country
-        .then(() => {
-            countriesDB.forEach(country => {
-                // merge current rates into country data
-                let rate = currency.find(value => value[0] === country.currency_code);
-                country.exchange_rate = rate[1];
+  client.query(SQL)
+    // First get the data from the SQL server
+    .then(results => countriesDB = results.rows)
+    // Use the SQL data to help get capitals and flags
+    .then(countries => getCapitalsAndFlags(countries)
+      .then(capsAndFlags => capitalsAndFlags = capsAndFlags))
+    // Get the current currency rates
+    .then(getCurrency()
+      .then(rates => currency = rates))
+    // Update data with the retrived information and calculate the current Big Mac Index for each country
+    .then(() => {
+      countriesDB.forEach(country => {
+        // merge current rates into country data
+        let rate = currency.find(value => value[0] === country.currency_code);
+        country.exchange_rate = rate[1];
 
-                // merge capitals and flags into data
-                let capitalFlag = capitalsAndFlags.find(value => value[0] === country.country_code);
-                country.capital = capitalFlag[1];
-                country.flag_url = capitalFlag[2];
+        // merge capitals and flags into data
+        let capitalFlag = capitalsAndFlags.find(value => value[0] === country.country_code);
+        country.capital = capitalFlag[1];
+        country.flag_url = capitalFlag[2];
 
-                //calculate Big Mac Index for USD in local country
-                country.usa_bmi = country.local_bmi / country.exchange_rate;
+        //calculate Big Mac Index for USD in local country
+        country.usa_bmi = country.local_bmi / country.exchange_rate;
 
-                //create an instance of each country with the constructor
-                Countries.allCountries.push(new Countries(country));
-            })
-        })
-        // Save updated data back to database
-        .then(() => updateCountryDb())
-        // Render the results of the updated information
-        .then(() => showExplore(request, response))
-        .catch(err => processErrors(err, response));
+        //create an instance of each country with the constructor
+        Countries.allCountries.push(new Countries(country));
+      })
+    })
+    // Save updated data back to database
+    .then(() => updateCountryDb())
+    // Render the results of the updated information
+    .then(() => showExplore(request, response))
+    .catch(err => processErrors(err, response));
 }
 
 // Saves the updated data back to the SQL Server
 function updateCountryDb() {
-    console.log('**** UPDATING SQL Database');
+  console.log('**** UPDATING SQL Database');
 
-    Countries.allCountries.forEach(country => {
-        let { id, country_name, capital, country_code, currency_code, exchange_rate, local_bmi, usa_bmi, flag_url, created_date } = country;
+  Countries.allCountries.forEach(country => {
+    let { id, country_name, capital, country_code, currency_code, exchange_rate, local_bmi, usa_bmi, flag_url, created_date } = country;
 
-        const SQL = `UPDATE countries SET country_name=$2, capital=$3, country_code=$4, currency_code=$5, exchange_rate=$6, local_bmi=$7, usa_bmi=$8, flag_url=$9, created_date=$10 WHERE id=$1;`;
+    const SQL = `UPDATE countries SET country_name=$2, capital=$3, country_code=$4, currency_code=$5, exchange_rate=$6, local_bmi=$7, usa_bmi=$8, flag_url=$9, created_date=$10 WHERE id=$1;`;
 
-        const values = [id, country_name, capital, country_code, currency_code, exchange_rate, local_bmi, usa_bmi, flag_url, created_date];
+    const values = [id, country_name, capital, country_code, currency_code, exchange_rate, local_bmi, usa_bmi, flag_url, created_date];
 
-        return client.query(SQL, values);
-    })
+    return client.query(SQL, values);
+  })
 }
 
 function showExplore(request, response) {
-    console.log('***** Preparing to render results');
+  console.log('***** Preparing to render results');
 
-    // sort the countries by Big Mac Index first
-    Countries.allCountries.sort((a, b) => a.usa_bmi - b.usa_bmi);
+  // sort the countries by Big Mac Index first
+  Countries.allCountries.sort((a, b) => a.usa_bmi - b.usa_bmi);
 
-    response.render('pages/explore/show', { countries: Countries.allCountries })
+  response.render('pages/explore/show', { countries: Countries.allCountries })
 }
 
 function getCountry(request, response) {
-    console.log('\n\n# Retrieiving the requested country');
+  console.log('\n\n# Retrieiving the requested country');
 
-    const requestedId = parseInt(request.params.id);
-    let countryDetail = Countries.allCountries.find(value => {
-        return value.id === requestedId;
-    })
-    console.log(countryDetail);
+  const requestedId = parseInt(request.params.id);
+  let countryDetail = Countries.allCountries.find(value => {
+    return value.id === requestedId;
+  })
+  console.log(countryDetail);
 
-    return response.render('pages/detail/show', { country: countryDetail });
+  return response.render('pages/detail/show', { country: countryDetail });
 
 }
 
-
 // Error Handler
 function processErrors(error, response) {
-    response.render('pages/404-error', { errorResult: error })
+  response.render('pages/404-error', { errorResult: error })
 }
