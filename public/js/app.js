60a086fc38405c004f4e569a4f9a738721ec4cb6
Ryan Milton <miltorm11@gmail.com>
2018-10-05 08:13:02 -0700

    pushing up jquery failure lol

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,5 @@
-'strict'
-
+console.log('app.js');
+$('.select').on('click', function(){
+  console.log('test');
+  alert('Please become a Premium User to use \'Save\' your results. Thank you.');
+});
