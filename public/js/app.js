8a42e7659fde01a2d50a7acdb94615b6a7d88d64
David Chambers <dlchambersjr@gmail.com>
2018-10-05 09:34:23 -0700

    Save Point after adjusting Deck CSS

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
+'use strict';
+
 console.log('app.js');
-$('.select').on('click', function(){
-  console.log('test');
-  alert('Please become a Premium User to use \'Save\' your results. Thank you.');
-});
+
+// $('.select').on('click', function(){
+//   console.log('test');
+//   alert('Please become a Premium User to use \'Save\' your results. Thank you.');
+// });
